cf905ea132bc70f2a49b0112e8ffa9367bfb79be
gminos <gjosebelarmino@gmail.com>
2026-09-12 22:37:41 -0500

    feat: add recursive sum of digits program

---
+++ ce_05_recursive_digit_sum.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define COLOR_GREEN "\033[32m"
+#define COLOR_YELLOW "\033[33m"
+#define COLOR_RESET "\033[0m"
+#define ZERO 0
+
+void get_value(int *number) {
+  printf(COLOR_YELLOW
+         "Enter a number to calculate the sum of its digits: " COLOR_RESET);
+  scanf("%d", number);
+
+  printf("---------------------------------------------------\n");
+}
+
+int sum_digits(int number, int accumulator) {
+  if (number / 10 == 0) {
+    return accumulator + number;
+  }
+
+  accumulator += number % 10;
+  number /= 10;
+
+  return sum_digits(number, accumulator);
+}
+
+void print_result(const int *number) {
+  printf(COLOR_GREEN "Sum of the digits: " COLOR_RESET);
+  printf("%d\n", sum_digits(*number, ZERO));
+}
+
+int main(void) {
+  int number;
+  get_value(&number);
+  print_result(&number);
+
+  return EXIT_SUCCESS;
+}
+
